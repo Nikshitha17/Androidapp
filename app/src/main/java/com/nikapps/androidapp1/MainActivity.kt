f55bbf1f9582735466f7ee0f73fe3fa9c8a3e7fc
Nikshitha17 <nikshithasivakumar@gmail.com>
2024-06-08 19:53:30 +0530

    Intents

--- app/src/main/java/com/nikapps/androidapp1/MainActivity.kt
+++ app/src/main/java/com/nikapps/androidapp1/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.nikapps.androidapp1
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}
 
-fun clickHandler(view: View) {
-    Log.i("MainActivity-clickhandler","button clicked")
+
+    fun clickHandler(view: View) {
+        Log.i("MainActivity-clickhandler", "button clicked")
+        var dialIntent: Intent =
+            Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
+        startActivity(dialIntent)
+    }
 }
