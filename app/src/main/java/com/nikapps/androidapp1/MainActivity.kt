38bb49b4e9c6d3f990cc369629292db30a5cce55
Nikshitha17 <nikshithasivakumar@gmail.com>
2024-06-13 18:06:03 +0530

    Activity Lifecycle

--- app/src/main/java/com/nikapps/androidapp1/MainActivity.kt
+++ app/src/main/java/com/nikapps/androidapp1/MainActivity.kt
@@ -1,24 +1,63 @@
 package com.nikapps.androidapp1
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 
 class MainActivity : AppCompatActivity() {
+    var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG,"activity is getting created")
     }
 
 
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG,"activity is started")
+    }
+
+    //sleep night -- incomming call ur activity goes into the background for sometime
+    override fun onPause() {
+        super.onPause()
+        Log.w(TAG,"activity has paused")
+
+    }
+
+    //death of the chick -- activity no lonnger exists in the ram -- pushed to hdd[hibernate]
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"activity is stopped")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG,"activity is destroyed")
+    }
+
     fun clickHandler(view: View) {
         Log.i("MainActivity-clickhandler", "button clicked")
-        var dialIntent: Intent =
-            Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
-        startActivity(dialIntent)
+//        var dialIntent: Intent =
+//            Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
+//        startActivity(dialIntent)
+        var hIntent:Intent = Intent(this,HomeActivity::class.java)
+        hIntent.putExtra("mykey","android-vit-nikshitha")
+        startActivity(hIntent)
+    }
+
+    fun inflateXml(){
+        var nameEditText = EditText(this)
+        nameEditText.setHint("enter ur name")
+        var pwdEditText = EditText(this)
+        pwdEditText.setHint("enter ur pass")
+        var loginButton = Button(this)
+        loginButton.setText("login")
     }
 }
