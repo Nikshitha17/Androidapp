d8af3220867e792a12ad0ca36e90e4895adb5b61
Nikshitha17 <nikshithasivakumar@gmail.com>
2024-06-20 20:15:05 +0530

    Put the parsed data into recyclerview

--- app/src/main/java/com/nikapps/androidapp1/HomeActivity.kt
+++ app/src/main/java/com/nikapps/androidapp1/HomeActivity.kt
@@ -4,22 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.TextView
 import com.nikapps.androidapp1.network.MarsApi
-import com.nikapps.androidapp1.network.MarsApiService
+import com.nikapps.androidapp1.network.MarsPhoto
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity(){
-    var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
+    var TAG = HomeActivity::class.java.simpleName //"HomeActivity"
+
+    lateinit var marsRecyclerView: RecyclerView
+    lateinit var marsAdapter: MarsAdapter
+    lateinit var photos:List<MarsPhoto>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        marsRecyclerView = findViewById(R.id.recyclerViewUrls)
+        marsRecyclerView.layoutManager = LinearLayoutManager(this)
+        photos = ArrayList()
+        marsAdapter = MarsAdapter(photos)
+        marsRecyclerView.adapter = marsAdapter
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -32,6 +44,8 @@ class HomeActivity : AppCompatActivity(){
     private fun getMarsPhotos() {
         GlobalScope.launch {
             var listMarsPhotos =   MarsApi.retrofitService.getPhotos()
+            photos = listMarsPhotos
+            marsAdapter.notifyDataSetChanged()
 //          var tvHome: TextView = findViewById(R.id.tvHome)
 //          tvHome.setText(listMarsPhotos.get(1).imgSrc)
             Log.i("homeactiviy",listMarsPhotos.size.toString())
