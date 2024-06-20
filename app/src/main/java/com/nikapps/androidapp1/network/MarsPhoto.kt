5dea93d9dac838d4720dd2d1d003d9adcb54c84b
Nikshitha17 <nikshithasivakumar@gmail.com>
2024-06-20 19:48:38 +0530

    Getting Json

---
+++ app/src/main/java/com/nikapps/androidapp1/network/MarsPhoto.kt
@@ -0,0 +1,17 @@
+package com.nikapps.androidapp1.network
+
+import com.squareup.moshi.Json
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+//serialization = baloon - defllate - inflate --make the object  transport ready
+//inflate -- deserialzation  -- autocad - serial nos on the floppy
+
+//break down your project into independently testable tasks -- small, discreet, independent
+
+data class MarsPhoto(
+    val id: String,
+    @Json(name = "img_src")
+    val imgSrc: String
+
+)
