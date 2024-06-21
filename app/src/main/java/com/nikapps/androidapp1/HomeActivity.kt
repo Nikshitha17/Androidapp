e7289175cb294ef242e59a184c90813b1dcc03f2
Nikshitha17 <nikshithasivakumar@gmail.com>
2024-06-21 11:17:57 +0530

    Databinding

--- app/src/main/java/com/nikapps/androidapp1/HomeActivity.kt
+++ app/src/main/java/com/nikapps/androidapp1/HomeActivity.kt
@@ -13,17 +13,17 @@ import kotlinx.coroutines.launch
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.Dispatchers
+import androidx.databinding.DataBindingUtil
 import com.nikapps.androidapp1.databinding.ActivityHomeBinding
-import okhttp3.Dispatcher
+import kotlinx.coroutines.Dispatchers
+
 
 class HomeActivity : AppCompatActivity(){
     var TAG = HomeActivity::class.java.simpleName //"HomeActivity"
 
 //  lateinit var marsRecyclerView: RecyclerView
     private lateinit var binding: ActivityHomeBinding
+    val photoMarsDatabinding = MarsPhoto("007","moonimage.com")
     lateinit var marsAdapter: MarsAdapter
     lateinit var photos:List<MarsPhoto>
 //  lateinit var imageView: ImageView
@@ -35,6 +35,8 @@ class HomeActivity : AppCompatActivity(){
 //        marsRecyclerView = findViewById(R.id.recyclerViewUrls)
 //        imageView = findViewById(R.id.imageView)
 //        marsRecyclerView.layoutManager = LinearLayoutManager(this)
+        val binding: ActivityHomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        binding.marsphotoxml = photoMarsDatabinding
         photos = ArrayList()
         marsAdapter = MarsAdapter(photos)
 //        marsRecyclerView.adapter = marsAdapter
